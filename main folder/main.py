322194038e1a4d624e1fa2c7a9e2a8c2aa863788
squidpod <benghgyt@gmail.com>
2024-12-22 23:35:04 +0100

    erste verison added, theoretische funktionsweise pro script run, muss noch auf forever upgegradet werden

--- main folder/main.py
+++ main folder/main.py
@@ -5,3 +5,33 @@
 
 import random
 
+max_chars = 13
+chosen_chars = []
+file = open("lol.txt", "w")
+
+def view():
+    k = chosen_chars
+    print(sorted(k))
+
+def make_char():
+    a = random.randint(1, max_chars)
+    if a in chosen_chars:
+        make_char()
+    else:
+        chosen_chars.append(a)
+        print("success!")
+def print_to_file():
+        file.write(str(sorted(chosen_chars)))
+
+#normal questioning
+while True:
+    cmd = input("select option: \n 1. 'view' for all entries \n 2. 'make_char' for new char")
+    if  cmd == "view" or "get_char":
+        if cmd == "view":
+            view()
+        elif cmd == "make_char":
+            make_char()
+        elif cmd == "print":
+            print_to_file()
+    else:
+        print("pls check for correct spelling or enter a valid command!")
