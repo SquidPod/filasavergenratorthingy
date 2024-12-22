abd2f2dc8d7d448a421e965282770fcc0169bdd2
squidpod <benghgyt@gmail.com>
2024-12-22 22:53:13 +0100

    first stuff added

--- main folder/main.py
+++ main folder/main.py
@@ -1,1 +1,7 @@
-print("hello world!")
+#this project is a little python script for mr modeegger
+#here is the goal that:
+# get a random number from 1 to 13, store this number somewhere, for each iteration, this number cant be chosen, maybe
+# signal if the iterattion is done, there should be an option to see all previous entrys, all entrys should be deleted if the iteration is done.
+
+import random
+
