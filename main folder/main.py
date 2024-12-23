768b08f6b7d70a87cb9da9eda14b44d80cd27ebb
squidpod <benghgyt@gmail.com>
2024-12-23 09:57:35 +0100

    significant changes, everything is printed to file, normal outputs, pretty much done

--- main folder/main.py
+++ main folder/main.py
@@ -1,38 +1,59 @@
 #this project is a little python script for mr modeegger
 #here is the goal that:
 # get a random number from 1 to 13, store this number somewhere, for each iteration, this number cant be chosen, maybe
-# signal if the iterattion is done, there should be an option to see all previous entrys, all entrys should be deleted if the iteration is done.
+# signal if the iteration is done, there should be an option to see all previous entrys, all entrys should be deleted if the iteration is done.
 
 import random
+import os
 
 max_chars = 13
-chosen_chars = []
+file_name = "lol.txt"
+
+# Initialize chosen_chars from file or create a new list
+try:
+    with open(file_name, "r") as file:
+        chosen_chars = list(map(int, file.read().strip().split(',')))
+except ValueError:
+    print("File content is invalid. Resetting entries.")
+    chosen_chars = []
+
 
 def view():
-    k = chosen_chars
-    print(sorted(k))
+    print("Current entries:", sorted(chosen_chars) if chosen_chars else "No entries yet.")
 
 def make_char():
-    a = random.randint(1, max_chars)
-    if a in chosen_chars:
-        make_char()
-    else:
-        chosen_chars.append(a)
-        print("success!")
+    while True:
+        new_char = random.randint(1, max_chars)
+        if new_char not in chosen_chars:
+            chosen_chars.append(new_char)
+            print(f"Generated character: {new_char}")
+            print_to_file()
+            break
+        else:
+            print(f"Character {new_char} already chosen. Retrying...")
 
 def print_to_file():
-    with open("lol.txt", "w") as file:
-        file.write(str(sorted(chosen_chars)))
+    with open(file_name, "w") as file:
+        file.write(','.join(map(str, sorted(chosen_chars))))
 
-# normal questioning
+def reset_entries():
+    global chosen_chars
+    chosen_chars = []
+    with open(file_name, "w") as file:
+        file.write("")
+    print("All entries have been cleared.")
+
+# Command interface
 while True:
-    cmd = input("select option: \n 1. 'view' for all entries \n 2. 'new' for new char \n 3. 'print' for printing onto txt\n")
-    if cmd == "view" or cmd == "new" or cmd == "print":
-        if cmd == "view":
-            view()
-        elif cmd == "new":
-            make_char()
-        else:
-            print_to_file()
+    cmd = input("Select an option: \n 1. 'view' to see all entries \n 2. 'new' to generate a new character \n 3. 'reset' to clear entries \n 4. 'exit' to quit\n").strip().lower()
+    if cmd == "view":
+        view()
+    elif cmd == "new":
+        make_char()
+    elif cmd == "reset":
+        reset_entries()
+    elif cmd == "exit":
+        print("Goodbye!")
+        break
     else:
-        print("pls check for correct spelling or enter a valid command!")
+        print("Invalid command. Please try again.")
